d60cadf5a1d9252864d51ffc34d97b0c984a964b
Victor Salles <victorgamesbr091@gmail.com>
2025-06-05 20:11:22 -0300

    fix: cobertura dos arquivos em resource, um arquivo de config e ajustes nos controllers para os testes.

--- src/main/java/br/edu/fatecsjc/lgnspringapi/dto/MarathonRequest.java
+++ src/main/java/br/edu/fatecsjc/lgnspringapi/dto/MarathonRequest.java
@@ -1,12 +1,14 @@
 package br.edu.fatecsjc.lgnspringapi.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class MarathonRequest {
   private String name;
   private String date;
---
+++ src/test/java/br/edu/fatecsjc/lgnspringapi/config/OpenApiConfigTest.java
@@ -0,0 +1,14 @@
+package br.edu.fatecsjc.lgnspringapi.config;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class OpenApiConfigTest {
+
+  @Test
+  void testInstance() {
+    OpenApiConfig config = new OpenApiConfig();
+    assertNotNull(config);
+  }
+}
